76a5a4db56927296f67cb88804742901001a9c34
Hexelena <phoenix.logd@gmx.de>
2016-05-04 02:00:15 +1200

    it is possible to locate the correct page from a query  there is no handling for non-greek strings yet

---
+++ greek.js
@@ -0,0 +1,171 @@
+"use strict";
+
+
+var simplerDict = {
+
+	// standard accents for Alpha
+	"ὰ": "α",
+	"ά": "α",
+	"ᾶ": "α",
+	// spiriti for Alpha
+	"ἀ": "α",
+	"ἁ": "α",
+	// spiriti and accents for Alpha
+	"ἄ": "α",
+	"ἅ": "α",
+	"ἂ": "α",
+	"ἃ": "α",
+	"ἇ": "α",
+	"ἆ": "α",
+
+	// Alpha with whateverthefuck 
+	// TODO: figure out what this is.
+	"ᾱ": "α",
+
+	// standard accents for Epsilon
+	"ὲ": "ε",
+	"έ": "ε",
+	// spiriti for Epsilon
+	"ἑ": "ε",
+	"ἐ": "ε",
+	// spiriti and accents for Epsilon
+	"ἓ": "ε",
+	"ἕ": "ε",
+	"ἒ": "ε",
+	"ἔ": "ε",
+
+	// standard accents for Eta
+	"ὴ": "η",
+	"ή": "η",
+	"ῆ": "η",
+	// spiriti for Eta
+	"ἠ": "η",
+	"ἡ": "η",
+	// spiriti and accents for Eta
+	"ἣ": "η",
+	"ἥ": "η",
+	"ἢ": "η",
+	"ἤ": "η",
+	"ἧ": "η",
+	"ἦ": "η",
+
+	// accents for Iota
+	"ὶ": "ι",
+	"ί": "ι",
+	"ῖ": "ι",
+	// spiriti for Iota
+	"ἱ": "ι",
+	"ἰ": "ι",
+	// spiriti and accents for Iota
+	"ἳ": "ι",
+	"ἵ": "ι",
+	"ἲ": "ι",
+	"ἴ": "ι",
+	"ἷ": "ι",
+	"ἶ": "ι",
+
+	// accents for Omikron
+	"ὸ": "ο",
+	"ό": "ο",
+	// spiriti for Omikron
+	"ὁ": "ο",
+	"ὀ": "ο",
+	// spiriti and accents for Omikron
+	"ὃ": "ο",
+	"ὅ": "ο",
+	"ὂ": "ο",
+	"ὄ": "ο",
+
+
+	// spiriti for Rho
+	"ῤ": "ρ",
+	"ῥ": "ρ",
+
+	// standard accents for Ypsilon
+	"ύ": "υ",
+	"ὺ": "υ",
+	"ῦ": "υ",
+	// spiriti for Ypsilon
+	"ὐ": "υ",
+	"ὑ": "υ",
+	// spiriti and accents for Ypsilon
+	"ὓ": "υ",
+	"ὕ": "υ",
+	"ὒ": "υ",
+	"ὔ": "υ",
+	"ὗ": "υ",
+	"ὖ": "υ",
+
+	// accents for Omega
+	"ὼ": "ω",
+	"ώ": "ω",
+	"ῶ": "ω",
+	// spiriti for Omega
+	"ὡ": "ω",
+	"ὠ": "ω",
+	// spiriti and accents for Omega
+	"ὣ": "ω",
+	"ὥ": "ω",
+	"ὢ": "ω",
+	"ὤ": "ω",
+	"ὧ": "ω",
+	"ὦ": "ω",
+
+	// #######################
+	// special simpilfications
+	// #######################
+
+	// hyphens are not needed
+	"-": "",
+	// strip whitespaces
+	" ": "",
+
+	// unify Sigmas
+	"ς": "σ"
+}
+
+var abc ="αβγδεζηϑικλμνξοπρστυφχψω";
+
+
+function normalizeGreek(inputString) {
+	var newString = "";
+
+	for (var i = 0; i < inputString.length; i++) {
+		if (inputString[i] in simplerDict) {
+			newString += simplerDict[inputString[i]];
+		} else {
+			newString += inputString[i];
+		}
+	}
+	return newString;
+}
+
+function isAfter(string, reference) {
+	var search = normalizeGreek(string);
+	var ref = normalizeGreek(reference);
+
+	if (search === ref) {
+		console.log("the strings are equal");
+		return true;
+	}
+
+	var i = 0;
+	while(true) {
+		//console.log("Entering Iteration No." + i + " of isAfter: (" + abc.indexOf(search[i]) + " vs " + abc.indexOf(ref[i]) + ").");
+		if (abc.indexOf(search[i]) > abc.indexOf(ref[i])) {
+			console.log("'" + search + "' comes after '" + ref + "' | -> true");
+			return true;
+		} else if (abc.indexOf(search[i]) < abc.indexOf(ref[i])) {
+			console.log("'" + search + "' comes before '" + ref + "' | -> false");
+			return false;
+		} else { // equal
+			i++;
+		}
+	}
+}
+
+module.exports = {
+	abc		: abc,
+	isAfter 		: isAfter,
+	normalizeGreek	: normalizeGreek
+};
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+"use strict";
 var express = require("express");
 var request = require("request");
 var cheerio = require("cheerio");
+
+var greek = require("./greek");
 
 var app = express();
 
@@ -12,70 +15,86 @@ var pageDict = [];
 
 // ROOT
 
-app.get("/", function(req, res) {
-	res.send("root of the agt-backend");
+app.get("/", function (req, res) {
+    res.send("root of the agt-backend");
 });
 
-// DICTIONARY
-app.get("/dict", function(req, res) {
-	console.log("incoming request");
+// DICTIONARY ROOT
+app.get("/dict", function (req, res) {
+    console.log("request for dict main page");
 
-	request(operoneurl, function(error, response, html) {
-		if (!error) {
-			var retmsg = "";
-			var $ = cheerio.load(html);
+    if (pageDict.length > 0) {
+        res.send(pageDict);
+    } else {
+        res.send("An Error occured. index scraping was unsuccesful!");
+    }
 
-			var relevantData = $("li a");
-			for (var i = 0; i < relevantData.length; i++) {
-				var link = relevantData[i].attribs.href;
-				var part = relevantData[i].children[0].data;
+});
 
-				console.log(i + ". : " + part + " -> " + link);
-				retmsg += i + ". : " + part + " -> " + link + "\n";
-
-			}
-			res.send(retmsg);
-		} else {
-			res.send("error receiving website");
-		}
-	})
-
+// DICTIONARY QUERY
+app.get("/dict/:query", function (req, res) {
+    console.log("incoming dict request for '" + req.params.query + "'. Processing as " + greek.normalizeGreek(req.params.query) + "'.");
+    res.send("received request for '" + req.params.query + "'. Searching for '" + greek.normalizeGreek(req.params.query) + "'.\nResult is possibly on page " + findPageLink(req.params.query));
 });
 
 function generatePageDict() {
-	
-	tempPageDict = [];
-	
-	request(operoneurl, function(error, response, html) {
-		if (!error) {
+
+    var tempPageDict = [];
+
+    request(operoneurl, function (error, response, html) {
+        if (!error) {
 
 
-			var $ = cheerio.load(html);
+            var $ = cheerio.load(html);
+            var relevantData = $("li a");
 
-			var relevantData = $("li a");
-			
-			for (var i = 0; i < relevantData.length; i++) {
-				var link = relevantData[i].attribs.href;
-				var part = relevantData[i].children[0].data;
-				var newObject = {
-					"part" : part,
-					"link" : link
-				};
-				//console.log(newObject);
-				tempPageDict.push(newObject);
-			}
-			pageDict = tempPageDict;
-		}
-	});
+            for (var i = 0; i < relevantData.length; i++) {
+                var link = relevantData[i].attribs.href;
+                var part = relevantData[i].children[0].data;
+
+                // small fix because operone starts with αλφα as first entry!
+                if (i === 0) {
+                    part = "α";
+                }
+
+                part = greek.normalizeGreek(part);
+
+                var newObject = {
+                    "begin" : part,
+                    "link" : link
+                };
+
+                tempPageDict.push(newObject);
+            }
+            pageDict = tempPageDict;
+        }
+    });
 }
+
+function findPageLink(query) {
+    var index = 0;
+
+    //console.log(greek.isAfter(query, pageDict[index].begin));
+    while(true) {
+        console.log(" " + index + " vs " + pageDict.length);
+        // the order of the expression evaluation is essential at this point.
+        // if the query is on the last page of the dictionary, the previous isAfter call returns true
+        // but if it is evaluated once more it will throw an error. Therefore the length condition must fail first!
+        if (index < pageDict.length && greek.isAfter(query, pageDict[index].begin)) {
+            index++;
+        } else {
+            index--;
+            break;
+        }
+    }
+    return pageDict[index].link;
+}
+
 
 app.listen(PORT);
 
+
 console.log("agt-backend started on port " + PORT);
 
-pageDict = generatePageDict();
-console.log("generated page dict");
-console.log(pageDict);
-setTimeout(function() {
-	console.log(pageDict);
-}, 3000);
+console.log("generating page dict");
+generatePageDict();
