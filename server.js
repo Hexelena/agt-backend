8a43730b1d41ebedaa2606ddfb6dbf42c9201bcf
Hexelena <phoenix.logd@gmx.de>
2016-05-25 16:48:06 +1200

    updated package.json to prepare for db instead of scraping lookup  removed old scraping methods from server.js

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 "use strict";
 var express = require("express");
-var request = require("request");
-var cheerio = require("cheerio");
 
 var greek = require("./greek");
 
@@ -9,11 +7,11 @@ var app = express();
 
 var PORT = 8081;
 
-var operoneurl = "http://localhost/web/phayax/agt/res/operone/altspr/wadinhalt.html";
-var operonebaseurl = "http://localhost/web/phayax/agt/res/operone/altspr/";
+//var operoneurl = "http://localhost/web/phayax/agt/res/operone/altspr/wadinhalt.html";
+//var operonebaseurl = "http://localhost/web/phayax/agt/res/operone/altspr/";
 
-var pageDict = [];
-var pageContent = [];
+//var pageDict = [];
+//var pageContent = [];
 
 // ROOT
 
@@ -25,92 +23,31 @@ app.get("/", function (req, res) {
 app.get("/dict", function (req, res) {
     console.log("request for dict main page");
 
-    if (pageDict.length > 0) {
-        res.send(pageDict);
-    } else {
-        res.send("An Error occured. index scraping was unsuccesful!");
-    }
-
+    //if (pageDict.length > 0) {
+    //    res.send(pageDict);
+    //} else {
+    //    res.send("An Error occured. index scraping was unsuccesful!");
+    //}
+    res.send('root of the dict lookup.')
 });
 
 // DICTIONARY QUERY
 app.get("/dict/:query", function (req, res) {
     console.log("incoming dict request for '" + req.params.query + "'. Processing as " + greek.normalizeGreek(req.params.query) + "'.");
     //res.send("received request for '" + req.params.query + "'. Searching for '" + greek.normalizeGreek(req.params.query) + "'.\nResult is possibly on page " + findPageLink(req.params.query));
-    var result = {
-        "origSearch" : req.params.query,
-        "convertedSearch" : greek.normalizeGreek(req.params.query),
-        "page" : findPageLink(req.params.query),
-        "pageContent" : ""
-    }
-    res.json(result);
+    // var result = {
+    //     "origSearch" : req.params.query,
+    //     "convertedSearch" : greek.normalizeGreek(req.params.query),
+    //     "page" : findPageLink(req.params.query),
+    //     "pageContent" : ""
+    // }
+    res.status(501).json({'error':'not implemented'});
 });
-
-function generatePageDict() {
-
-    var tempPageDict = [];
-
-    request(operoneurl, function (error, response, html) {
-        if (!error) {
-
-
-            var $ = cheerio.load(html);
-            var relevantData = $("li a");
-
-            for (var i = 0; i < relevantData.length; i++) {
-                var link = relevantData[i].attribs.href;
-                var part = relevantData[i].children[0].data;
-
-                // small fix because operone starts with αλφα as first entry!
-                if (i === 0) {
-                    part = "α";
-                }
-
-                part = greek.normalizeGreek(part);
-
-                var newObject = {
-                    "begin" : part,
-                    "link" : link
-                };
-
-                tempPageDict.push(newObject);
-            }
-            pageDict = tempPageDict;
-            
-        }
-    });
-}
-
-function findPageLink(query) {
-    var index = 0;
-
-    // iterate to find the page in which to search the query string
-    // proceed by increasing until it is one step too far. Then go back once and return the value.
-    while(index < pageDict.length && greek.isAfter(query, pageDict[index].begin)) {
-        index++;
-    }
-
-    index--;
-
-    return pageDict[index].link;
-}
-
-function getPageContent(pageLink) {
-    request(pageLink, function(error, response, html) {
-        if (!error) {
-            var $ = cheerio.load(html);
-
-            var relevantData = $("ul li");
-
-        }
-    });
-}
-
 
 app.listen(PORT);
 
 
 console.log("agt-backend started on port " + PORT);
 
-console.log("generating page dict");
-generatePageDict();
+//console.log("generating page dict");
+//generatePageDict();
