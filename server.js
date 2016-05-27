8ddb7e9a034abec73d9eac4282eb063a172ac20e
Hexelena <phoenix.logd@gmx.de>
2016-05-28 11:25:08 +1200

    added more info to /dict route.  added the :mode parameter to the query route (now: /dict/:mode/:query)  added an if else if clause to differentiate between precise and rough lookups. This was necessary since it is not possible to use a parameter a column name.

--- server.js
+++ server.js
@@ -18,40 +18,59 @@ var db = new sqlite3.Database('agt.sqlite');
 // ROOT
 
 app.get("/", function (req, res) {
+    console.log("GET / - root of the agt-backend")
     res.send("root of the agt-backend");
 });
 
 // DICTIONARY ROOT
 app.get("/dict", function (req, res) {
-    console.log("request for dict main page");
-
-    //if (pageDict.length > 0) {
-    //    res.send(pageDict);
-    //} else {
-    //    res.send("An Error occured. index scraping was unsuccesful!");
-    //}
-    res.send('root of the dict lookup.')
+    console.log("GET /dict - dict main page");
+    db.get('SELECT COUNT(*) FROM pagecontent;', function(err, rows) {
+        res.json({
+            'info': {
+                'description': 'root of the dict lookup.',
+                'entries': rows['COUNT(*)'],
+                'lookup-modes': ['precise','rough'],
+                'lookup-url': '/dict/<mode>/<query>'
+        }
+            })
+    });
 });
 
 // DICTIONARY QUERY
-app.get("/dict/:query", function (req, res) {
+app.get("/dict/:mode/:query", function (req, res) {
+    // logging
     console.log("incoming dict request for '" + req.params.query + "'. Processing as " + greek.normalizeGreek(req.params.query) + "'.");
-    db.all('SELECT * FROM pagecontent WHERE roughword LIKE ? ORDER BY LENGTH(greek) ASC LIMIT 10;',[req.params.query + '%'], function(err, rows) {
-        if (err) {
-            console.log('error: ' + err);
-        }
-        res.json(rows)
-    });
-    //res.send("received request for '" + req.params.query + "'. Searching for '" + greek.normalizeGreek(req.params.query) + "'.\nResult is possibly on page " + findPageLink(req.params.query));
-    // var result = {
-    //     "origSearch" : req.params.query,
-    //     "convertedSearch" : greek.normalizeGreek(req.params.query),
-    //     "page" : findPageLink(req.params.query),
-    //     "pageContent" : ""
-    // }
-    //res.status(501).json({'error':'not implemented'});
+
+    // parameter error handling
+    // FIXME: somehow write that if expression better.
+    var mode = req.params.mode;
+    if (!(mode === 'precise' || mode == 'rough')) {
+        res.status(400).json({'error': "unknown mode + '" + req.params.mode + "'' !"});
+        return;
+    }
+    // processing the request
+    // the query searches for a word starting with the query 
+    // then orders them by length (assuming that words that have a more similar length to the query are more relevant)
+    // then limits them (for now)
+    if (req.params.mode === "rough") {
+        db.all('SELECT * FROM pagecontent WHERE roughword LIKE ? ORDER BY LENGTH(greek) ASC LIMIT 10;',[req.params.query + '%'], function(err, rows) {
+            if (err) {
+                console.log('error: ' + err);
+            }
+            res.json(rows)
+        });
+    } else if (req.params.mode === "precise") {
+        db.all('SELECT * FROM pagecontent WHERE preciseword LIKE ? ORDER BY LENGTH(greek) ASC LIMIT 10;',[req.params.query + '%'], function(err, rows) {
+            if (err) {
+                console.log('error: ' + err);
+            }
+            res.json(rows)
+        });
+    }
 });
 
+// off we go!
 app.listen(PORT);
 
 console.log("agt-backend started on port " + PORT);
